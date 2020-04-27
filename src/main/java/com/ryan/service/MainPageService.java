4bb219984a3be08b0b5ba7466dcb3c30f176ab21
jjh <pursue503@naver.com>
2020-04-27 13:53:52 +0900

    Add MainPage Service Tests

--- src/main/java/com/ryan/service/MainPageService.java
+++ src/main/java/com/ryan/service/MainPageService.java
@@ -1,6 +1,15 @@
 package com.ryan.service;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.ryan.domain.EBookVO;
+
 public interface MainPageService {
 	
+	public ArrayList<EBookVO> getEbookList(String type, String[] keyword);
+	
+	public ArrayList<EBookVO> getBookList(String type, String[] keyword);
 	
 }
