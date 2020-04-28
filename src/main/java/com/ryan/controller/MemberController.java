a70c9ab6d12d03b71e9db4a91e1957f626c474e3
JUNGMIYOUNG <miyou_@naver.com>
2020-04-28 14:56:01 +0900

    detailInfo update

--- src/main/java/com/ryan/controller/MemberController.java
+++ src/main/java/com/ryan/controller/MemberController.java
@@ -72,7 +72,7 @@ public class MemberController {
 		
 		Map<String, Boolean> resultMap = new HashMap<String, Boolean>(); 
 		
-		if(emailService.insertEmailCode(email)) { // DB에 인증정보 입력성공시 PK 키 리턴.. 
+		if(emailService.insertEmailCode(email)) { // DB에 인증정보 입력성공시 PK키 리턴.. 
 			if(emailService.authenticationCodeSend(email)) { //메일보내기 성공하면
 				resultMap.put("result", true);
 				return resultMap;
