0268d436da872e5492b6477b37e3f68a06d89c53
no28-jsp <480ej@naver.com>
2020-05-07 11:27:56 +0900

    Update Follow Service
    delete memberfollowVo

--- src/main/java/com/ryan/controller/FollowController.java
+++ src/main/java/com/ryan/controller/FollowController.java
@@ -1,14 +1,22 @@
 package com.ryan.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.ArrayList;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ryan.domain.member.FollowVO;
+import com.ryan.domain.member.MemberVO;
 import com.ryan.service.member.FollowService;
 
 import lombok.Setter;
 
-@Controller
+@RestController
 @RequestMapping("/follow/*")
 public class FollowController {
 	
@@ -16,10 +24,52 @@ public class FollowController {
 	private FollowService service;
 	
 	@RequestMapping("/request")
-	public String requestFollow() {
-		
-		
-		return null;
+	public Boolean requestFollow(@RequestParam("following") String following, HttpSession session ) {
+		MemberVO member = (MemberVO) session.getAttribute("ryanMember");
+		// return false == 이미 팔로우 한사람
+		// 검증을 위해서
+		if(!service.followCheck(member.getMemberEmail(), following)) {
+			if(service.insertFollow(member.getMemberEmail(), following)) {
+				return true;
+			} else {
+				return false;
+			}
+		} else { 
+			return false;
+		}
+	
 	}
+	
+	
+	@PostMapping("/delete")
+	public Boolean deleteFollow(@RequestParam("following") String following, HttpSession session) {
+		MemberVO member = (MemberVO) session.getAttribute("ryanMember");
+		
+		if(!service.followCheck(member.getMemberEmail(), following)) {
+			if(service.deleteFollow(member.getMemberEmail(), following)) {
+				return true;
+			}else {
+				return false;
+			}
+		}else {
+			return false;
+		}
+	}
+	
+	//내가 팔로우한 사람들 보여주기
+	@PostMapping("/myfollowing")
+	public ArrayList<FollowVO> getMyFollowing(HttpSession session){
+		MemberVO member = (MemberVO) session.getAttribute("ryanMember");
+		return service.getMyFollowing(member);
+	}
+	
+	@PostMapping("/myfollower")
+	public ArrayList<FollowVO> getMyFollower(HttpSession session){
+		MemberVO member = (MemberVO) session.getAttribute("ryanMember");
+		return service.getMyFollower(member);
+	}
+	
+	
+	
 
 }
--- src/main/java/com/ryan/service/member/FollowServiceImpl.java
+++ src/main/java/com/ryan/service/member/FollowServiceImpl.java
@@ -1,16 +1,48 @@
 package com.ryan.service.member;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ryan.domain.member.FollowVO;
+import com.ryan.domain.member.MemberVO;
+import com.ryan.mapper.FollowMapper;
+
 import lombok.Setter;
+
+
 
 @Service
 public class FollowServiceImpl implements FollowService{
 
 	@Setter(onMethod_ = {@Autowired})
 	private FollowMapper mapper;
-	
+
+	@Override
+	public Boolean insertFollow(String follower, String following) {
+		return mapper.insertFollow(follower, following)==1 ? true : false;
+	}
+
+	@Override
+	public Boolean deleteFollow(String follower, String following) {
+		return mapper.deleteFollow(follower, following) ==1 ? true : false;
+	}
+
+	@Override
+	public ArrayList<FollowVO> getMyFollowing(MemberVO member) {
+		return mapper.getMyFollowing(member);
+	}
+
+	@Override
+	public ArrayList<FollowVO> getMyFollower(MemberVO member) {
+		return mapper.getMyFollower(member);
+	}
+
+	@Override
+	public Boolean followCheck(String follower, String following) {
+		return mapper.followCheck(follower, following)>=1? true : false;
+	}
 	
 	
 	
