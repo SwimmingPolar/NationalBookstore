81190ddcdedc39ddfe6bbe3c8798603fe54daf60
no28-jsp <480ej@naver.com>
2020-05-08 15:11:10 +0900

    Add Alarm System
    Add services, controllers, mappers, VO

---
+++ src/test/java/com/ryan/mapper/MyBookAlarmMapperTests.java
@@ -0,0 +1,32 @@
+package com.ryan.mapper;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ryan.domain.book.BookAlarmVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class MyBookAlarmMapperTests {
+
+	@Setter(onMethod_ = {@Autowired})
+	private MyBookAlarmMapper mapper;
+	
+	@Test
+	public void showAlarm() {
+		String email = "abc1234@naver.com";
+		ArrayList<BookAlarmVO> list = mapper.showAlarm(email);
+		for(BookAlarmVO vo : list) {
+			log.info(vo);
+		}
+	}
+}
