000843c27d62cc776d007a203b908403543f99d1
jjh <pursue503@naver.com>
2020-05-09 19:01:47 +0900

    Fix pom.xml

--- src/main/java/com/ryan/controller/ReviewController.java
+++ src/main/java/com/ryan/controller/ReviewController.java
@@ -1,9 +1,7 @@
 package com.ryan.controller;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
