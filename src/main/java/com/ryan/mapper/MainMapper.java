17c6728e30d358d0d67de9774b8a65765a1c626b
jjh <pursue503@naver.com>
2020-05-11 11:36:45 +0900

    Add MainPage Mapper.xml

--- src/main/java/com/ryan/mapper/MainMapper.java
+++ src/main/java/com/ryan/mapper/MainMapper.java
@@ -12,8 +12,21 @@ public interface MainMapper {
 	
 	public ArrayList<EBookVO> getBookList(@Param("type") String type, @Param("keyword") String[] keyword);
 	
+	//메인페이지 데이터
 	
+	//오늘의 추천 도서
+	public ArrayList<EBookVO> getTodayBookList();
 	
+	//해시태그 추천 (구상중)
+	
+	//사람들이 많이 읽은 책
+	public ArrayList<EBookVO> getBestReadBook();
+	
+	//베스트셀러
+	public ArrayList<EBookVO> getBestSeller();
+	
+	//세일
+	public ArrayList<EBookVO> getDisCountBook();
 	
 	
 }
