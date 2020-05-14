56d5e2be6575143b40a1d58eb16fea5fce9a736c
sim.sim <sim@sim.aaa>
2020-05-14 15:22:41 +0900

    notice,eunquiry update

--- src/main/java/com/admin/service/board/NoticeBoardServiceImpl.java
+++ src/main/java/com/admin/service/board/NoticeBoardServiceImpl.java
@@ -1,14 +1,17 @@
 package com.admin.service.board;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.admin.domain.board.FileVO;
 import com.admin.domain.board.NoticeBoardVO;
 import com.admin.domain.board.PageVO;
+import com.admin.mapper.FileBoardMapper;
 import com.admin.mapper.NoticeBoardMapper;
 
 @Service
@@ -17,6 +20,9 @@ public class NoticeBoardServiceImpl implements NoticeBoardService{
 	@Autowired
 	private NoticeBoardMapper mapper;
 	
+	@Autowired
+	private FileBoardMapper fileMapper;
+	
 	@Override
 	public boolean noticeWrite(NoticeBoardVO notice) {
 		return mapper.insertNotice(notice)>0?true:false;
@@ -24,6 +30,11 @@ public class NoticeBoardServiceImpl implements NoticeBoardService{
 	
 	@Override
 	public boolean noticeDelete(NoticeBoardVO notice) {
+		if(mapper.numChk(notice.getNoticeNo())>0) {
+			//fileMapper.deleteAllFiles(enquiry.getBoardNum());
+			//return mapper?true:false;
+		}else
+			return false;
 		return mapper.deleteNotice(notice)>0?true:false;
 	}
 	
@@ -48,5 +59,25 @@ public class NoticeBoardServiceImpl implements NoticeBoardService{
 	public ArrayList<FileVO> selectNoticeFileList(int boardNum) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public boolean insertFiles(NoticeBoardVO notice, ArrayList<MultipartFile> files, String path) throws IOException {
+		
+		return false;
+	}
+
+	@Override
+	public boolean deleteFile(int fileNum) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public NoticeBoardVO selectNotice(int noticeNo) {
+		if(mapper.numChk(noticeNo)>0)
+			return mapper.selectNotice(noticeNo);
+		else
+			return null;
 	}
 }
